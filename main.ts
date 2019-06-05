17db27dfbe0b029696c0255b946f26da80a5cd55
Trevor Shaw <shawt@genlrn.com>
2019-06-05 10:07:57 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -81,9 +81,9 @@ namespace encMotor {
      * Moves a robot based on wheel rotations
      * @param rt indicates number of rotations eg:4
      */
-    //% block="move (%robot) %dir for %rt Rotations"
+    //% block="move %robot %dir for %rt Rotations"
     
-    export function drive(dir: motorDir, rt: number) {
+    export function drive(robot: Robot,dir: motorDir, rt: number) {
         switch (dir) {
             case 0:
                 forward(50, rt);
