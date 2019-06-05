89c2f77aca25b032f04a1d658a8424f3157dc35f
Trevor Shaw <shawt@genlrn.com>
2019-06-05 09:45:04 -0400

    reDesign robot obj

--- main.ts
+++ main.ts
@@ -2,7 +2,12 @@
 //% color="#AA278D"
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 pins.setEvents(DigitalPin.P0, PinEventType.Pulse)
-let ticks: number = 0
+let lTicks: number = 0
+let rTicks: number = 0
+let lTurns: number = 0.0
+let rTurns: number = 0.0
+let lenc: DigitalPin
+let renc: DigitalPin
 enum motorChoice {
     //% block="left"
     Left,
@@ -23,17 +28,17 @@ enum motorDir {
 
 enum encPin {
     //% block="P0"
-    p0,
+    P0 = DigitalPin.P0,
     //% block=P1"
-    p1,
+    P1 = DigitalPin.P1,
     //% block="P2"
-    p2,
+    P2 = DigitalPin.P2,
     //% block="P8"
-    p8,
+    P8 = DigitalPin.P8,
     //% block="P12"
-    p12,
+    P12 = DigitalPin.P12,
     //% blcok="P14"
-    p14
+    P14 = DigitalPin.P14
 }
 
 enum MotorPower {
@@ -43,18 +48,42 @@ enum MotorPower {
     Off = 28672
 }
 
-
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_EVT_ANY, function () {
-    ticks += 1
+pins.onPulsed(lenc, PulseValue.High, function () {
+    lTicks += 1
 })
+
+pins.onPulsed(renc, PulseValue.High, function () {
+    rTicks += 1
+})
+
+
 namespace encMotor {
 
+    //%
+    export class Robot { }
+    let _ratio: number
+    let _lenc: DigitalPin
+    let _renc: DigitalPin
+    /**
+     * Creates a robot and automtically set it to a variable
+     * @param ratio gives the motor to wheel turn ratio eg:48
+     */
+    //% block="create robot with %ratio to 1 gearing"
+    //% blockSetVariable=robot
+    export function createRobot(ratio: number): Robot {
+        _ratio = ratio;
+        _lenc = DigitalPin.P0;
+        _renc = DigitalPin.P1
+        return undefined;
+    }
 
-    //% blockID="encodedMotor" block="Encoded Motor|Enc:Wheel Ratio $ratio Lenc $le Renc $re Motor $mc Direction $dir Speed $sp Rotations $rt"
+
+    //% blockID="encodedMotor" block="Drive %robot Direction $dir Speed $sp Rotations $rt"
     //% ratio.defl=48
     //% sp.defl=50
-    export function drive(ratio: number, le: encPin, re: encPin, mc: motorChoice, dir: motorDir, sp: number, rt: number) {
-        if (mc == 2) {
+    export function drive(dir: motorDir, sp: number, rt: number) {
+
+        
             switch (dir) {
                 case 0:
                     forward(sp, rt);
@@ -66,7 +95,7 @@ namespace encMotor {
                     spin(sp, rt);
                     break;
             }
-        }
+        
     }
 
     function forward(sp: number, rt: number) {
@@ -81,7 +110,7 @@ namespace encMotor {
         pins.i2cWriteNumber(89, MotorPower.On, NumberFormat.Int16BE) //enable motors
     }
 
-    function pwr(dir: number, speed: number): number{
+    function pwr(dir: number, speed: number): number {
         let outPwr: number = 0
         let pwr = 0
         speed = Math.abs(speed)
