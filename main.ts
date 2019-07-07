23d6ab74a9ae85e2bd8ab02df6b0ae2e821df394
Trevor Shaw <shawt@genlrn.com>
2019-07-07 17:27:27 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -112,7 +112,7 @@ namespace encMotor {
             motorGo(_baseSp, 8192, dir) //start right motor
         }
         else { motorGo(50, motor, dir) }
-        while (_lTurns < (rt + .125) && _rTurns < (rt + .125)) {
+        while (_lTurns < (rt + .12) && _rTurns < (rt + .12)) {
             correction = (_lTicks - _rTicks) / _kp;
             motorGo(_baseSp + correction, 8448, dir) //correct left motor 
             basic.pause(100)
