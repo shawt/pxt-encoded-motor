5af37b351d68b3ca0f56e3618f5a0081a331b3b3
Trevor Shaw <shawt@genlrn.com>
2019-07-07 21:17:57 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -153,7 +153,7 @@ namespace encMotor {
         _rTicks = 0;
     }
 
-    //% block="drive %motorChoice motor(s) %motorDir with power %power"
+    //% block="drive %motorChoice motor(s) %motorDir with %power"
     //% power.min = 0 power.max = 100 power.defl = 50
     export function driveIndef(motor: motorChoice, dir: motorDir, power: number) {
         stop();
