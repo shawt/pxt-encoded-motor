95bace57ba6a1a0efac7514e669e6b5598142b89
Trevor Shaw <shawt@genlrn.com>
2019-07-09 22:38:40 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -130,21 +130,19 @@ namespace encMotor {
         else {
             motorGo(50, motor, dir)
         }
-        while ((_rTurns < rt + .05) && (_lTurns < rt + .05)) {
-            basic.pause(100)
+       
+
+         while ((_lTurns < (rt + .05)) && (_rTurns < (rt + .05))) {
+             _rerrTicks = 0;
+             _lerrTicks = 0;
+             basic.pause(150)
+         if (motor == motorChoice.Both) {
+             correction = (_rerrTicks - _lerrTicks) / _kp;
+             lSpeed += correction;
+             motorGo(lSpeed, 8448, dir) //correct left motor
         }
 
-        // while ((_lTurns < (rt + .05)) && (_rTurns < (rt + .05))) {
-        // if (motor == motorChoice.Both) {
-        //     _rerrTicks = 0;
-        //     _lerrTicks = 0;
-        //     basic.pause(150)
-        //     correction = (_rerrTicks - _lerrTicks) / _kp;
-        //     lSpeed += correction;
-        //     motorGo(lSpeed, 8448, dir) //correct left motor
-        //}
-
-        //}
+        }
         stop()
 
 
